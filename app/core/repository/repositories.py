7f1a119c982961fff070cdbed9d9e2b593830e2a
Manoel Veríssimo dos Santos Neto <verissimo.manoel@gmail.com>
2022-11-03 15:09:51 -0300

    feat: Add first version of project.

---
+++ app/core/repository/repositories.py
@@ -0,0 +1,23 @@
+"""Repositories module."""
+from abc import ABC, abstractmethod
+from typing import Iterator
+
+from app.core.models import User
+
+
+class IUserRepository(ABC):
+    @abstractmethod
+    def get_all(self) -> Iterator[User]:
+        raise NotImplemented("The method get_all needs to be implemented by all subclasses!")
+
+    def get_by_id(self, user_id: int) -> User:
+        raise NotImplemented("The method get_by_id needs to be implemented by all subclasses!")
+
+    def add(self, email: str, password: str, is_active: bool = True) -> User:
+        raise NotImplemented("The method add needs to be implemented by all subclasses!")
+
+    def delete_by_id(self, user_id: int) -> None:
+        raise NotImplemented("The method delete_by_id needs to be implemented by all subclasses!")
+
+
+
